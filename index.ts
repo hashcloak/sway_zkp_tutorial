d5b97d4b737cae06cae0f4de313996ca093f99c6
LYC386 <alex98605@gmail.com>
2025-03-05 14:06:10 +0800

    add support to groth16 & fflonk

--- index.ts
+++ index.ts
@@ -1,69 +1,184 @@
-import {bn, Provider, Wallet} from "fuels";
-import { SwayZkpExampleFactory } from "./fuels-out";
+import { bn, Provider, Wallet } from "fuels"
+import proof_json from "./proof.json"
+import publicSignals from "./public.json"
+import { SwayZkpExampleFactory } from "./fuels-out"
 
 const main = async () => {
-  const PROVIDER_URL = "http://127.0.0.1:4000/v1/graphql";
-  const PRIVATE_KEY = "0xde97d8624a438121b86a1956544bd72ed68cd69f2c99555b08b1e8c51ffd511c";
+
+  const PROVIDER_URL = "http://127.0.0.1:4000/v1/graphql"
+  // Default funded account on local fuel-core
+  const PRIVATE_KEY = "0xde97d8624a438121b86a1956544bd72ed68cd69f2c99555b08b1e8c51ffd511c"
 
   const provider = new Provider(PROVIDER_URL);
   const wallet = Wallet.fromPrivateKey(PRIVATE_KEY, provider);
 
   const factory = new SwayZkpExampleFactory(wallet);
-  const { contract } = await (await factory.deploy()).waitForResult();
+  const { contract } = await (await factory.deploy()).waitForResult()
 
-  // Construct Proof object
-  const proof = {
-    proof_A: {
-      x: bn("0x12c3cd0f11c9b934d977b1003d3fb8104668e7470ed696df8a39e24376ae8165", "hex"),
-      y: bn("0x274981cb79590f9d99388c6d3c826c09dfaadfde680c80d3cff82c6abd6baedb", "hex")
-    },
-    proof_B: {
-      x: bn("0x1204b8a0f4ea28c688182f7d63f2a4c5551f690db5ff568d57893f5e3df837c2", "hex"),
-      y: bn("0x2ff31ba6fcf8eae165a43c34ff5e75b900731cbe2a64bde76dae2cde93e2304b", "hex")
-    },
-    proof_C: {
-      x: bn("0x08878cd99520b967e632c0c0673bbcdb4563ec539b92986c22c95f3576cbb7c9", "hex"),
-      y: bn("0x075945f899f308411bf42b1c633b2de244853c7abfb4ac77162fce1c0801de98", "hex")
-    },
-    proof_Z: {
-      x: bn("0x06fe399400102e20b845a5ee2f742a5bcfa62a52a6011c3cd4a01d195a66d775", "hex"),
-      y: bn("0x1e380b3ec7bbffd930eaa2021861159db94d4fa809ccefc7791e5311c8141c8e", "hex")
-    },
-    proof_T1: {
-      x: bn("0x0ecdf61d2e843502206fa21bbbc576840c2b0e5d29e536f3525f5f20c1b9053a", "hex"),
-      y: bn("0x051fb7ad2720be2ff38ecd78e777a62933afb6ad11c1aa46eefb95f5aaf976c4", "hex")
-    },
-    proof_T2: {
-      x: bn("0x22cb35ffe3b91ac8e46a0f7e175331b2e207de06f81fc026725b0f61ade6b562", "hex"),
-      y: bn("0x115143a9e7d68fdc6ad8bee1d6b2f6304cd742ba16dda5d4ebff2117df00681d", "hex")
-    },
-    proof_T3: {
-      x: bn("0x2cadb1a8225027a7a383ca9a09a2bf0e1c854f60a9c05454a292abecc2d1245c", "hex"),
-      y: bn("0x14f3c8936aca8c53311500df3dcb17a7dd05477b0d9f4b31390b5cc47a270158", "hex")
-    },
-    proof_Wxi: {
-      x: bn("0x162c82f57ac8638da82f552f33ef832234d1fad1087704aa6d624d2d8d92c72e", "hex"),
-      y: bn("0x2df37d8d040bd42c6ddf8b65b0b072a6443d2bd3a6394386f5e41e8ef211fe1d", "hex")
-    },
-    proof_Wxiw: {
-      x: bn("0x1d6dbcf9383b0e95c87d01e2edd72fc570bb62a9601ab6dd1a8f2d7a42269158", "hex"),
-      y: bn("0x1de5b4e3f6cecf9251c425cddfb8222c290cd8e039adab8cdc9fe5433020f5f8", "hex")
-    },
-    eval_a: bn("0x0b2c3627d350016d7a39347fc6d5abebfc6bc99918334b839616e398fea0d864", "hex"),
-    eval_b: bn("0x2c1749db395adc2eca5fc2c3448a057e2b9a24f2503665a7f7af7c3dfe0c118b", "hex"),
-    eval_c: bn("0x045ee44a614dca07534e8290cfddcdd60b3fd99e96d4d534cc3e6df297275b41", "hex"),
-    eval_s1: bn("0x098dc4e934c0b9c0f8173631def06d873c0b7dd5746df900c251dd953860dcd7", "hex"),
-    eval_s2: bn("0x010ad58e674c3369948e1e607fa207f75aba069558fc9c1384df7e4870d0a744", "hex"),
-    eval_zw: bn("0x2228ecd0cf41c4439af138aa0d04e9ba990da8aac5cfa70857261ff1c01944c1", "hex")
-  };
+  switch (proof_json.protocol) {
+    case "groth16":
+      {
+        const a = proof_json.pi_a.map((v) => {
+          return bn(v);
+        }).slice(0, 2)
 
-  const pubInput = [
-      bn("0x0000000000000000000000000000000000000000000000000000000000000021", "hex")
-  ];
+        const b = proof_json.pi_b.map((v) => {
+          return v.toReversed().map((v) => {
+            return bn(v);
+          })
+        }).slice(0, 2)
 
-  const { waitForResult } = await contract.functions.verify(proof, pubInput).call();
-  const { value } = await waitForResult();
-  console.log("result is:", value);
-};
+        const c = proof_json.pi_c.map((v) => {
+          return bn(v);
+        }).slice(0, 2)
+
+        const publicSignalBN = publicSignals.map((v) => {
+          return bn(v);
+        })
+
+        const { waitForResult } = await contract.functions.verify(a, b, c, publicSignalBN).call();
+        const res = await waitForResult();
+        console.log("result is: ", res.value);
+        break;
+      }
+    case "plonk":
+      {
+        const proof_A = {
+          x: proof_json.A[0],
+          y: proof_json.A[1],
+        };
+        const proof_B = {
+          x: proof_json.B[0],
+          y: proof_json.B[1],
+        };
+        const proof_C = {
+          x: proof_json.C[0],
+          y: proof_json.C[1],
+        };
+        const proof_Z = {
+          x: proof_json.Z[0],
+          y: proof_json.Z[1],
+        };
+        const proof_T1 = {
+          x: proof_json.T1[0],
+          y: proof_json.T1[1],
+        };
+
+        const proof_T2 = {
+          x: proof_json.T2[0],
+          y: proof_json.T2[1],
+        };
+        const proof_T3 = {
+          x: proof_json.T3[0],
+          y: proof_json.T3[1],
+        };
+        const proof_Wxi = {
+          x: proof_json.Wxi[0],
+          y: proof_json.Wxi[1],
+        };
+        const proof_Wxiw = {
+          x: proof_json.Wxiw[0],
+          y: proof_json.Wxiw[1],
+        };
+        const eval_a = bn(proof_json.eval_a);
+        const eval_b = bn(proof_json.eval_b);
+        const eval_c = bn(proof_json.eval_c);
+        const eval_s1 = bn(proof_json.eval_s1);
+        const eval_s2 = bn(proof_json.eval_s2);
+        const eval_zw = bn(proof_json.eval_zw);
+        const proof = {
+          proof_A,
+          proof_B,
+          proof_C,
+          proof_Z,
+          proof_T1,
+          proof_T2,
+          proof_T3,
+          proof_Wxi,
+          proof_Wxiw,
+          eval_a,
+          eval_b,
+          eval_c,
+          eval_s1,
+          eval_s2,
+          eval_zw
+        };
+        const pubInput = publicSignals.map((v) => {
+          return bn(v);
+        });
+        const { waitForResult } = await contract.functions.verify(proof, pubInput).call();
+        const { value } = await waitForResult();
+        console.log("result is:", value);
+        break;
+      }
+    case "fflonk":
+      {
+        const C1 = {
+          x: proof_json.polynomials.C1[0],
+          y: proof_json.polynomials.C1[1],
+        };
+        const C2 = {
+          x: proof_json.polynomials.C2[0],
+          y: proof_json.polynomials.C2[1],
+        };
+        const W = {
+          x: proof_json.polynomials.W1[0],
+          y: proof_json.polynomials.W1[1],
+        };
+        const W_dash = {
+          x: proof_json.polynomials.W2[0],
+          y: proof_json.polynomials.W2[1],
+        };
+        const q_L = { x: bn(proof_json.evaluations.ql) };
+        const q_R = { x: bn(proof_json.evaluations.qr) };
+        const q_M = { x: bn(proof_json.evaluations.qm) };
+        const q_O = { x: bn(proof_json.evaluations.qo) };
+        const q_C = { x: bn(proof_json.evaluations.qc) };
+        const S_sigma_1 = { x: bn(proof_json.evaluations.s1) };
+        const S_sigma_2 = { x: bn(proof_json.evaluations.s2) };
+        const S_sigma_3 = { x: bn(proof_json.evaluations.s3) };
+        const a = { x: bn(proof_json.evaluations.a) };
+        const b = { x: bn(proof_json.evaluations.b) };
+        const c = { x: bn(proof_json.evaluations.c) };
+        const z = { x: bn(proof_json.evaluations.z) };
+        const z_omega = { x: bn(proof_json.evaluations.zw) };
+        const T_1_omega = { x: bn(proof_json.evaluations.t1w) };
+        const T_2_omega = { x: bn(proof_json.evaluations.t2w) };
+        const batch_inv = { x: bn(proof_json.evaluations.inv) };
+        const proof = {
+          C1,
+          C2,
+          W,
+          W_dash,
+          q_L,
+          q_R,
+          q_M,
+          q_O,
+          q_C,
+          S_sigma_1,
+          S_sigma_2,
+          S_sigma_3,
+          a,
+          b,
+          c,
+          z,
+          z_omega,
+          T_1_omega,
+          T_2_omega,
+          batch_inv,
+        };
+        const pubInput = publicSignals.map((v) => {
+          return bn(v);
+        })
+        const { waitForResult } = await contract.functions.verify(proof, pubInput).call();
+        const { value } = await waitForResult();
+        console.log("result is:", value);
+        break;
+      }
+    default:
+      console.log("verifier not supported")
+
+  }
+}
 
 main();
